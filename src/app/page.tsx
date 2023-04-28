435fbe6347d0f343977b921698e1929577845b3d
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2023-04-28 14:20:36 -0400

    feat: :sparkles: adding six fav albums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Home as HomeIcon, Search, Library } from 'lucide-react'
-
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -34,9 +34,45 @@ export default function Home() {
             <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
           </nav>
         </aside>
-        <main className="flex-1 p-6">main</main>
-      </div>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="p-1 rounded-full bg-black/40">
+              <ChevronLeft />
+            </button>
+            <button className="p-1 rounded-full bg-black/40">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="font-semibold text-4xl mt-10">Good Afternoon</h1>
+          <div className='grid grid-cols-3 gap-4 mt-40'>
+            <a className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album Wasting Light" />
+              <strong>Wasting Light</strong>
+            </a>
+            <a className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album Wasting Light" />
+              <strong>Wasting Light</strong>
+            </a>
+            <a className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album Wasting Light" />
+              <strong>Wasting Light</strong>
+            </a>
+            <a className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album Wasting Light" />
+              <strong>Wasting Light</strong>
+            </a>
+            <a className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album Wasting Light" />
+              <strong>Wasting Light</strong>
+            </a>
+            <a className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album Wasting Light" />
+              <strong>Wasting Light</strong>
+            </a>
+          </div>
+        </main>
+      </div >
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
-    </div>
+    </div >
   )
 }
