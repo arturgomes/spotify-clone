ff5c42e33bf757ce30555203f8dc6f6a82afb2b9
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2023-04-28 14:02:01 -0400

    feat: :sparkles: sidebar finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+import { Home as HomeIcon, Search, Library } from 'lucide-react'
+
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav>
-
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+          </div>
+          <nav className="space-y-5 mt-10">
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <HomeIcon /> Home
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+            </a>
+          </nav>
+          <nav className="mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3 ">
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">Playlist 1</a>
           </nav>
         </aside>
         <main className="flex-1 p-6">main</main>
